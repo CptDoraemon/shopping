2a50c5ba20f67047d162cfe9b620572362a398e9
CptDoraemon <yxx0827@gmail.com>
2019-12-03 20:50:11 -0500

    Updates

---
+++ precache-manifest.be20a46410c631061c7c9cf64ac1e392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cd3b49cf7f7dcf67a0d5769f427b08",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "8625e07978995e77fabb",
+    "url": "/shopping/static/css/main.255b3e30.chunk.css"
+  },
+  {
+    "revision": "4ef2e7e3dcc124267131",
+    "url": "/shopping/static/js/2.de33a69e.chunk.js"
+  },
+  {
+    "revision": "8625e07978995e77fabb",
+    "url": "/shopping/static/js/main.907c74d3.chunk.js"
+  },
+  {
+    "revision": "121ce3b13fd6d48f7b84",
+    "url": "/shopping/static/js/runtime-main.93a8ae92.js"
+  }
+]);
